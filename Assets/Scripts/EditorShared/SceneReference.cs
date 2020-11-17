4ccb71d02a55ccf612c6535eb2a567073f7479fa
Jonatan Johansson <smeas@users.noreply.github.com>
2020-11-17 21:20:19 +0100

    Improve SceneReference

--- Assets/Scripts/EditorShared/SceneReference.cs
+++ Assets/Scripts/EditorShared/SceneReference.cs
@@ -1,55 +1,63 @@
 //
 // Author: Jonatan Johansson
+// Created: XX-XX-2018/19
+// Updated: 17-11-2020
 //
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Object = UnityEngine.Object;
+using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
-
 #endif
 
 /// <summary>
-/// Represents a reference to a scene which you can select from the editor.
+/// A reference to a scene which you can select from the inspector.
 /// </summary>
 [Serializable]
-public class SceneReference : ISerializationCallbackReceiver {
+public struct SceneReference : ISerializationCallbackReceiver {
 	[SerializeField, HideInInspector]
-	private string scenePath = "";
+	private int buildIndex;
 
 #if UNITY_EDITOR
-	[SerializeField]
-	private Object sceneAsset;
-
-	private string GetScenePathFromAsset() =>
-		sceneAsset as SceneAsset != null ? AssetDatabase.GetAssetPath(sceneAsset) : "";
+	[SerializeField] private SceneAsset sceneAsset;
 #endif
+
+	public int BuildIndex {
+		get {
+		#if UNITY_EDITOR
+			return SceneUtility.GetBuildIndexByScenePath(ScenePath);
+		#else
+			return buildIndex;
+		#endif
+		}
+	}
 
 	public string ScenePath {
 		get {
 		#if UNITY_EDITOR
-			return GetScenePathFromAsset();
+			if (sceneAsset != null)
+				return AssetDatabase.GetAssetPath(sceneAsset);
+			return "";
 		#else
-			return scenePath;
+			return SceneUtility.GetScenePathByBuildIndex(buildIndex);
 		#endif
 		}
 	}
 
 	public void OnBeforeSerialize() {
 	#if UNITY_EDITOR
-		// Update the scenePath field before serialization.
-		scenePath = GetScenePathFromAsset();
+		// Update the buildIndex field before serialization so that it will be correct in builds.
+		buildIndex = BuildIndex;
 	#endif
 	}
 
 	public void OnAfterDeserialize() { }
 
-	// Implicit conversion to string so that it can be passed directly to SceneManager.LoadScene(string) etc.
-	public static implicit operator string(SceneReference sceneReference) =>
-		sceneReference.ScenePath;
+	// Implicit conversion to int so that it can be used directly with SceneManager.LoadScene(int) etc.
+	public static implicit operator int(SceneReference sceneReference) => sceneReference.BuildIndex;
 }
 
 
@@ -83,20 +91,16 @@ namespace PropertyDrawers {
 				}
 				else if (!sceneInfo.enabled) {
 					GUI.backgroundColor = Color.yellow;
-					label.tooltip = $"*Disabled, build index: {sceneInfo.buildIndex}* " + label.tooltip;
+					label.tooltip = $"*Disabled at build index: {sceneInfo.buildIndex}* " + label.tooltip;
 				}
 				else {
 					GUI.backgroundColor = Color.green;
-					label.tooltip = $"*Enabled, build index: {sceneInfo.buildIndex}* " + label.tooltip;
+					label.tooltip = $"*Enabled at build index: {sceneInfo.buildIndex}* " + label.tooltip;
 				}
 			}
 
 			// Draw property.
-			label = EditorGUI.BeginProperty(position, label, property);
-			sceneAssetProperty.objectReferenceValue =
-				EditorGUI.ObjectField(position, label, sceneAssetProperty.objectReferenceValue,
-				                      typeof(SceneAsset), false) as SceneAsset;
-			EditorGUI.EndProperty();
+			EditorGUI.PropertyField(position, sceneAssetProperty, label);
 
 			// Restore color.
 			GUI.backgroundColor = oldColor;
@@ -108,21 +112,69 @@ namespace PropertyDrawers {
 
 			if (hasScene) {
 				if (sceneInfo.InBuildSettings)
-					menu.AddItem(new GUIContent("Remove scene from build settings"), false, RemoveFromBuild,
-					             sceneAssetProperty);
+					menu.AddItem(new GUIContent("Remove scene from build settings"), false, RemoveFromBuild, sceneAssetProperty);
 				else
 					menu.AddItem(new GUIContent("Add scene to build settings"), false, AddToBuild, sceneAssetProperty);
 
 				if (sceneInfo.InBuildSettings)
-					menu.AddItem(new GUIContent("Enabled in build"), sceneInfo.enabled, ToggleEnableInBuild,
-					             sceneAssetProperty);
+					menu.AddItem(new GUIContent("Enabled in build"), sceneInfo.enabled, ToggleEnableInBuild, sceneAssetProperty);
 			}
 
 			menu.ShowAsContext();
 		}
 
 
+		//
+		// Context actions
+		//
+
+		private static void AddToBuild(object arg) {
+			((SerializedProperty)arg).serializedObject.Update();
+			if (!TryGetSceneFromProperty((SerializedProperty)arg, out SceneInfo sceneInfo)) return;
+			if (!sceneInfo.InBuildSettings) {
+				EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes
+					.Append(new EditorBuildSettingsScene(sceneInfo.path, true)).ToArray();
+				EditorBuildSettings.scenes = buildScenes;
+
+				Debug.Log($"Added scene: '{sceneInfo.asset.name}' ({sceneInfo.path}) to build settings at build index: {buildScenes.Length - 1}.");
+			}
+		}
+
+		private static void RemoveFromBuild(object arg) {
+			SerializedProperty property = (SerializedProperty)arg;
+			property.serializedObject.Update();
+			if (!TryGetSceneFromProperty(property, out SceneInfo scene)) return;
+			if (!scene.InBuildSettings) return;
+
+			List<EditorBuildSettingsScene> buildScenes = EditorBuildSettings.scenes.ToList();
+			int index = scene.buildIndex;
+			if (index < buildScenes.Count && buildScenes[index].path == scene.path) {
+				buildScenes.RemoveAt(index);
+				EditorBuildSettings.scenes = buildScenes.ToArray();
+
+				Debug.Log($"Removed scene: '{scene.asset.name}' ({scene.path}) with build index: {index} from build settings.");
+			}
+		}
+
+		private static void ToggleEnableInBuild(object arg) {
+			SerializedProperty property = (SerializedProperty)arg;
+			property.serializedObject.Update();
+			if (!TryGetSceneFromProperty(property, out SceneInfo scene)) return;
+			if (!scene.InBuildSettings) return;
+
+			EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
+			int index = scene.buildIndex;
+			if (index < buildScenes.Length && buildScenes[index].path == scene.path) {
+				buildScenes[index].enabled = !scene.enabled;
+				EditorBuildSettings.scenes = buildScenes;
+
+				Debug.Log($"{(buildScenes[index].enabled ? "Enabled" : "Disabled")} scene: '{scene.asset.name}' ({scene.path}) with build index: {index} in build settings.");
+			}
+		}
+
+		//
 		// Helpers
+		//
 
 		private struct SceneInfo {
 			public SceneAsset asset;
@@ -153,58 +205,9 @@ namespace PropertyDrawers {
 			sceneInfo = default;
 			var sceneAsset = property.objectReferenceValue as SceneAsset;
 			if (sceneAsset == null) return false;
+
 			sceneInfo = GetSceneInfo(sceneAsset);
 			return sceneInfo.IsValid;
-		}
-
-		// Context actions
-
-		private static void AddToBuild(object arg) {
-			((SerializedProperty)arg).serializedObject.Update();
-			if (!TryGetSceneFromProperty((SerializedProperty)arg, out SceneInfo sceneInfo)) return;
-			if (!sceneInfo.InBuildSettings) {
-				EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes
-					.Append(new EditorBuildSettingsScene(sceneInfo.path, true)).ToArray();
-				EditorBuildSettings.scenes = buildScenes;
-
-				Debug.Log(
-					$"Added scene: '{sceneInfo.asset.name}' ({sceneInfo.path}) to build settings at build index: {buildScenes.Length - 1}.");
-			}
-		}
-
-		private static void RemoveFromBuild(object arg) {
-			SerializedProperty property = (SerializedProperty)arg;
-			property.serializedObject.Update();
-			if (!TryGetSceneFromProperty(property, out SceneInfo scene)) return;
-			if (!scene.InBuildSettings) return;
-
-			List<EditorBuildSettingsScene> buildScenes = EditorBuildSettings.scenes.ToList();
-			int index = scene.buildIndex;
-			if (index < buildScenes.Count && buildScenes[index].path == scene.path) {
-				buildScenes.RemoveAt(index);
-				EditorBuildSettings.scenes = buildScenes.ToArray();
-
-				Debug.Log(
-					$"Removed scene: '{scene.asset.name}' ({scene.path}) with build index: {index} from build settings.");
-			}
-		}
-
-		private static void ToggleEnableInBuild(object arg) {
-			SerializedProperty property = (SerializedProperty)arg;
-			property.serializedObject.Update();
-			if (!TryGetSceneFromProperty(property, out SceneInfo scene)) return;
-			if (!scene.InBuildSettings) return;
-
-
-			EditorBuildSettingsScene[] buildScenes = EditorBuildSettings.scenes;
-			int index = scene.buildIndex;
-			if (index < buildScenes.Length && buildScenes[index].path == scene.path) {
-				buildScenes[index].enabled = !scene.enabled;
-				EditorBuildSettings.scenes = buildScenes;
-
-				Debug.Log(
-					$"{(buildScenes[index].enabled ? "Enabled" : "Disabled")} scene: '{scene.asset.name}' ({scene.path}) with build index: {index} in build settings.");
-			}
 		}
 	}
 }
