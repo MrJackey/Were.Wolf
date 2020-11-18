9267b3ea59372bd682110bae2a0d90767e6bfe92
MrJackey <jaclun2003@skola.goteborg.se>
2020-11-18 13:22:14 +0100

    Player no longer stick to walls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] private float maxSpeed = 3;
 
 	[Header("Jumping")]
-	[SerializeField] private float jumpForce = 7.5f;
+	[SerializeField] private float jumpVelocity = 7.5f;
 
 	private Rigidbody2D rb2D;
 	private BoxCollider2D boxCollider;
@@ -30,6 +30,9 @@ public class PlayerController : MonoBehaviour {
 		isGrounded = CheckIfGrounded();
 		float xInput = Input.GetAxisRaw("Horizontal");
 
+		if (rb2D.velocity.x == 0)
+			velocity.x = 0;
+
 		if (xInput != 0) {
 			float newVelocityX = velocity.x + xInput * acceleration * Time.deltaTime;
 			velocity.x = Mathf.Clamp(newVelocityX, -maxSpeed, maxSpeed);
@@ -45,7 +48,7 @@ public class PlayerController : MonoBehaviour {
 		rb2D.velocity = new Vector2(velocity.x, rb2D.velocity.y);
 
 		if (doJump) {
-			rb2D.velocity = new Vector2(velocity.x, jumpForce);
+			rb2D.velocity = new Vector2(velocity.x, jumpVelocity);
 			doJump = false;
 		}
 	}
@@ -55,7 +58,7 @@ public class PlayerController : MonoBehaviour {
 
 		int floorLayer = LayerMask.GetMask("Ground");
 		Vector2 rayStartPosition = new Vector2(position.x, position.y - boxCollider.bounds.extents.y);
-		float rayDistance = 0.025f;
+		float rayDistance = 0.05f;
 
 		RaycastHit2D hit = Physics2D.Raycast(rayStartPosition,Vector2.down, rayDistance, floorLayer);
 
