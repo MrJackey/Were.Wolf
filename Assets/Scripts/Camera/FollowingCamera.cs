f3142676427821b4cfd7b76358a003e783d7e33b
Jonatan Johansson <smeas@users.noreply.github.com>
2020-11-18 14:09:00 +0100

    Add some camera prototypes

---
+++ Assets/Scripts/Camera/FollowingCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour {
+	[SerializeField] private Transform target = null;
+	[SerializeField] private float smoothTime = 0.15f;
+	[SerializeField] private float maxSpeed = float.PositiveInfinity;
+
+	private Vector2 offset;
+	private Vector2 currentVelocity;
+
+	private void Start() {
+		offset = transform.position - target.position;
+	}
+
+	private void LateUpdate() {
+		Vector3 currentPosition = transform.position;
+		Vector2 targetPosition = (Vector2)target.position + offset;
+		Vector2 newPosition = Vector2.SmoothDamp(currentPosition, targetPosition, ref currentVelocity, smoothTime, maxSpeed);
+		transform.position = new Vector3(newPosition.x, newPosition.y, currentPosition.z);
+	}
+}
---
+++ Assets/Scripts/Camera/SnappingCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class SnappingCamera : MonoBehaviour {
+	[SerializeField] private Transform target = null;
+
+	[Header("Smoothing")]
+	[SerializeField] private bool enableSmoothing = true;
+	[SerializeField] private float transitionDuration = 0.15f;
+
+	private new Camera camera;
+	private Vector3 startPosition;
+	private Vector3 currentVelocity;
+
+	private void Start() {
+		camera = GetComponent<Camera>();
+		startPosition = transform.position;
+
+		if (!camera.orthographic)
+			Debug.LogWarning($"{nameof(SnappingCamera)} only works on orthographic cameras!");
+	}
+
+	private void LateUpdate() {
+		Vector3 targetCameraPosition = CalculateTargetPosition();
+		if (enableSmoothing)
+			transform.position = Vector3.SmoothDamp(transform.position, targetCameraPosition,
+			                                        ref currentVelocity, transitionDuration);
+		else
+			transform.position = targetCameraPosition;
+	}
+
+	private Vector3 CalculateTargetPosition() {
+		Rect cameraRect = GetCameraWorldRect(camera);
+		Vector2 targetPositionInGrid = (Vector2)target.position + cameraRect.size / 2f;
+		Vector2 gridCell = new Vector2(
+			Mathf.Floor(targetPositionInGrid.x / cameraRect.size.x),
+			Mathf.Floor(targetPositionInGrid.y / cameraRect.size.y));
+
+		return startPosition + (Vector3)(gridCell * cameraRect.size);
+	}
+
+	private static Rect GetCameraWorldRect(Camera camera) {
+		Rect viewRect = camera.rect;
+		return new Rect {
+			min = camera.ViewportToWorldPoint(new Vector3(viewRect.xMin, viewRect.yMin, 0)),
+			max = camera.ViewportToWorldPoint(new Vector3(viewRect.xMax, viewRect.yMax, 0)),
+		};
+	}
+}
