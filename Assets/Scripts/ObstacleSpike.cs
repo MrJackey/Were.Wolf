1b52160c1755fe315fbed5720fa6535f1a2a29da
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-11-18 15:26:43 +0100

    obstacleSpike prefabs and script clean code

--- Assets/Scripts/ObstacleSpike.cs
+++ Assets/Scripts/ObstacleSpike.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ObstacleSpike : MonoBehaviour
-{
-	private void Start(){
-		
-	}
+public class ObstacleSpike : MonoBehaviour {
+	public void OnCollisionEnter2D(Collision2D collision) {
 
-	public void OnCollisionEnter2D(Collision2D collision){
-
-		if(collision.gameObject.tag == "Player"){
+		if (collision.gameObject.CompareTag("Player")) {
 			Scene currentScene = SceneManager.GetActiveScene();
 			SceneManager.LoadScene(currentScene.name);
 		}
