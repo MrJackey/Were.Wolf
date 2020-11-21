c4554ba3238b51a7b3c296076a9bebeee17169f3
Jonatan Johansson <smeas@users.noreply.github.com>
2020-11-21 21:23:11 +0100

    Add math utilities

---
+++ Assets/Scripts/Utils/MathX.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class MathX {
+	public static Vector2 Floor(Vector2 vec) {
+		return new Vector2(Mathf.Floor(vec.x), Mathf.Floor(vec.y));
+	}
+
+	public static Vector2 Ceil(Vector2 vec) {
+		return new Vector2(Mathf.Ceil(vec.x), Mathf.Ceil(vec.y));
+	}
+}
