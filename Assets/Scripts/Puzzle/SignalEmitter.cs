0ecfb82eb69eacc63b951c390865fedb9b197c09
MrJackey <jaclun2003@skola.goteborg.se>
2020-11-23 10:24:45 +0100

    Created Signal Emitter

---
+++ Assets/Scripts/Puzzle/SignalEmitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalEmitter : MonoBehaviour {
+	[SerializeField, PropertySetter(nameof(IsActivated))] private bool isActivated = false;
+
+	private UnityEvent onActivationChange = new UnityEvent();
+
+	public bool IsActivated {
+		get => isActivated;
+		set {
+			if (value == isActivated)
+				return;
+
+			isActivated = value;
+			onActivationChange.Invoke();
+		}
+	}
+
+	public UnityEvent OnActivationChange => onActivationChange;
+}
