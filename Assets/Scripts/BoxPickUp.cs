efa413085fc9611c45ac35bd01ac2c4e4ab1ee0b
TosaLipkiss <tovahl2002@skola.goteborg.se>
2020-11-23 15:00:54 +0100

    BoxPuzzle script
    script, prefab placeholder box.

---
+++ Assets/Scripts/BoxPickUp.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPickUp : MonoBehaviour {
+
+	[SerializeField] private BoxCollider2D boxCollider, boxTrigger;
+	[SerializeField] public GameObject playerHand;
+
+	private Rigidbody2D rb2d;
+
+	[SerializeField] private bool readyToPickUp = false, isPickedUp = false;
+
+	private void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.isKinematic = false;
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.E)) {
+			if (readyToPickUp) {
+				isPickedUp = true;
+				boxCollider.enabled = false;
+				boxTrigger.enabled = false;
+				rb2d.isKinematic = true;
+
+				transform.parent = playerHand.transform;
+				transform.localPosition = Vector3.zero;
+			}
+			else if(isPickedUp) {
+				boxCollider.enabled = true;
+				boxTrigger.enabled = true;
+				rb2d.isKinematic = false;
+				isPickedUp = false;
+
+				transform.parent = null;
+			}
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.CompareTag("Player")) {
+			readyToPickUp = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision) {
+		if(collision.CompareTag("Player")) {
+			readyToPickUp = false;
+		}
+	}
+}
