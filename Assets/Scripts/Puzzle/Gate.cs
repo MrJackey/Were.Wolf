06484c739be1314f5ccd591166bb661a3257a23f
MrJackey <jaclun2003@skola.goteborg.se>
2020-11-25 13:40:41 +0100

    Camera can now pan towards the gate

--- Assets/Scripts/Puzzle/Gate.cs
+++ Assets/Scripts/Puzzle/Gate.cs
@@ -3,25 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gate : SignalReceiver {
-	private static readonly int openHash = Animator.StringToHash("open");
-	private static readonly int closeHash = Animator.StringToHash("close");
+	private static readonly int isOpenHash = Animator.StringToHash("isOpen");
 
 	[SerializeField] private bool isOpen;
 	[SerializeField] private Animator animator;
 	[SerializeField] private bool panCamera;
+	[SerializeField] private float showDuration = 1f;
+
+	private new SnappingCamera camera;
+	private Transform player;
+	private PlayerController playerController;
+	private float cameraTransitionMultiplier = 0.10f;
 
 	private void Awake() {
 		if (isOpen)
 			Open();
+
+		GameObject playerObj = GameObject.FindWithTag("Player");
+		player = playerObj.transform;
+		playerController = playerObj.GetComponent<PlayerController>();
+		camera = Camera.main.GetComponent<SnappingCamera>();
 	}
 
 	public void Open() {
 		isOpen = true;
-		animator.SetTrigger(openHash);
+
+		if (panCamera && camera != null)
+			StartCoroutine(ShowEvent());
+		else
+			animator.SetBool(isOpenHash, isOpen);
 	}
 
 	public void Close() {
 		isOpen = false;
-		animator.SetTrigger(closeHash);
+
+		if (panCamera && camera != null)
+			StartCoroutine(ShowEvent());
+		else
+			animator.SetBool(isOpenHash, isOpen);
+	}
+
+	private IEnumerator ShowEvent() {
+		playerController.AllowControls = false;
+		float oldTransDur = camera.TransitionDuration;
+		float newTransDur = oldTransDur * cameraTransitionMultiplier * Vector2.Distance(transform.position, player.transform.position);
+
+		camera.TransitionDuration = newTransDur;
+		camera.Target = transform;
+
+		yield return new WaitForSeconds(newTransDur * 2f);
+		animator.SetBool(isOpenHash, isOpen);
+
+		yield return new WaitForSeconds(showDuration);
+		camera.Target = player.transform;
+		camera.TransitionDuration = oldTransDur;
+		playerController.AllowControls = true;
 	}
 }
