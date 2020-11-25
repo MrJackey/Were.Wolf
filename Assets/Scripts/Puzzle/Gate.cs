a38175fc2de59f0f8dc344cafecfd9fd3fe79b4e
MrJackey <jaclun2003@skola.goteborg.se>
2020-11-25 11:40:49 +0100

    Implemented gate functionality

---
+++ Assets/Scripts/Puzzle/Gate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : SignalReceiver {
+	private static readonly int openHash = Animator.StringToHash("open");
+	private static readonly int closeHash = Animator.StringToHash("close");
+
+	[SerializeField] private bool isOpen;
+	[SerializeField] private Animator animator;
+	[SerializeField] private bool panCamera;
+
+	private void Awake() {
+		if (isOpen)
+			Open();
+	}
+
+	public void Open() {
+		isOpen = true;
+		animator.SetTrigger(openHash);
+	}
+
+	public void Close() {
+		isOpen = false;
+		animator.SetTrigger(closeHash);
+	}
+}
