b03d7fe305efc520dd792476350b9580fb082d43
MrJackey <jaclun2003@skola.goteborg.se>
2020-12-01 11:47:46 +0100

    Spikes knockback on hit

--- Assets/Scripts/Common/DamageArea.cs
+++ Assets/Scripts/Common/DamageArea.cs
@@ -13,6 +13,7 @@ public class DamageArea : MonoBehaviour {
 	[SerializeField] private float damage = 10;
 	[SerializeField] private float damageCooldown = 0;
 	[SerializeField] private UnityEvent<Vector2> onDamageEffect;
+	[SerializeField] private float knockbackForce = 5;
 
 	[Header("Filters")]
 	[SerializeField] private LayerMask layerMask = -1;
@@ -39,6 +40,8 @@ public class DamageArea : MonoBehaviour {
 		if (ignoreTriggers && other.isTrigger) return;
 		if (layerMask != -1 && ((1 << other.gameObject.layer) & layerMask.value) == 0) return;
 
+		CalculateAvgContact(other, out Vector2 averageContact);
+
 		Health healthComponent = other.attachedRigidbody.GetComponent<Health>();
 		if (healthComponent != null) {
 			cooldownTimer = damageCooldown;
@@ -46,25 +49,30 @@ public class DamageArea : MonoBehaviour {
 			health.TakeDamage(isEnter ? damage : damage * Time.deltaTime);
 
 			if (other.attachedRigidbody.CompareTag(effectTarget))
-				DoDamageEffect(other);
+				onDamageEffect.Invoke(averageContact);
+		}
+
+		Knockbackable knockbackComponent = other.attachedRigidbody.GetComponent<Knockbackable>();
+		if (knockbackComponent != null) {
+			Vector2 direction = (Vector2) other.transform.position - averageContact;
+			knockbackComponent.Knockback(direction.normalized, knockbackForce, 0.2f);
 		}
 	}
 
-	private void DoDamageEffect(Collider2D other) {
+	private void CalculateAvgContact(Collider2D other, out Vector2 avg) {
 		int contactCount = other.GetContacts(contacts);
-		if (contactCount < 1) return;
-
-		Vector2 averageContact = new Vector2();
+		avg = new Vector2();
 		int num = 0;
+		if (contactCount < 1) return;
 
 		for (int i = 0; i < contactCount; i++) {
 			if (contacts[i].collider.gameObject != gameObject) continue;
 
-			averageContact += contacts[i].point;
+			avg += contacts[i].point;
 			num++;
 		}
 
 		if (num > 0)
-			onDamageEffect.Invoke(averageContact / num);
+			avg /= num;
 	}
 }
