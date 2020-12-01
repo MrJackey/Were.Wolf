24632c0aecd46e98be9a98fef7b344ab474631d5
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-01 19:27:33 +0100

    Add in-game music and temporary music player script

---
+++ Assets/Scripts/Audio/LevelMusicPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class LevelMusicPlayer : MonoBehaviour {
+	[SerializeField] private SceneHelper sceneHelper;
+
+	private AudioSource audioSource;
+	private bool isPlaying;
+
+	private void Start() {
+		audioSource = GetComponent<AudioSource>();
+
+		if (sceneHelper.CurrentLevel != -1)
+			Play();
+	}
+
+	private void OnEnable() {
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnDisable() {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (sceneHelper.CurrentLevel != -1)
+			Play();
+		else
+			Stop();
+	}
+
+	public void Play() {
+		if (isPlaying) return;
+		audioSource.Play();
+		isPlaying = true;
+	}
+
+	public void Stop() {
+		audioSource.Stop();
+		isPlaying = false;
+	}
+}
