dacc358f44e090ed3d4abe4cce35de5d792de713
MrJackey <jaclun2003@skola.goteborg.se>
2020-12-01 11:47:20 +0100

    Created knockbackable script

---
+++ Assets/Scripts/Common/Knockbackable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Knockbackable : MonoBehaviour {
+	[SerializeField] private UnityEvent onKnockback;
+	[SerializeField] private UnityEvent onKnockbackEnd;
+
+	private Coroutine knockbackRoutine;
+	private bool doKnockback;
+	private Vector2 velocity;
+	private Rigidbody2D rb2D;
+
+	public Vector2 Velocity => velocity;
+
+	private void Start() {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	public void Knockback(Vector2 direction, float force, float duration) {
+		onKnockback.Invoke();
+		velocity = direction * force;
+
+		if (knockbackRoutine != null)
+			StopCoroutine(knockbackRoutine);
+
+		doKnockback = true;
+		knockbackRoutine = StartCoroutine(CoKnockbackDuration(duration));
+	}
+
+	private void FixedUpdate() {
+		if (doKnockback)
+			rb2D.velocity = velocity;
+	}
+
+	private IEnumerator CoKnockbackDuration(float duration) {
+		yield return new WaitForSeconds(duration);
+		onKnockbackEnd.Invoke();
+		knockbackRoutine = null;
+		doKnockback = false;
+	}
+}
