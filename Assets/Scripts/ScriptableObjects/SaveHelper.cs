32101442334fd78b2d42363b278132d74e368094
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-01 10:17:46 +0100

    Add save helper

---
+++ Assets/Scripts/ScriptableObjects/SaveHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Save Helper", menuName = "Game/Save Helper", order = 0)]
+public class SaveHelper : ScriptableObject {
+	[SerializeField] private SceneHelper sceneHelper;
+
+	public void MarkCurrentLevelCompleted() {
+		if (sceneHelper.CurrentLevel != -1)
+			MarkLevelCompleted(sceneHelper.CurrentLevel);
+	}
+
+	public void MarkLevelCompleted(int levelIndex) {
+		SaveManager.Instance.Data.currentLevel = Mathf.Max(SaveManager.Instance.Data.currentLevel, levelIndex);
+		SaveManager.Instance.Save();
+	}
+}
