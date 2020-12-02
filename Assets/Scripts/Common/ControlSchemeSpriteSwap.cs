8b31adfb981b9043f565706dc964885a2d3092da
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-02 19:00:37 +0100

    Add script to swap sprites based on control scheme

---
+++ Assets/Scripts/Common/ControlSchemeSpriteSwap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlSchemeSpriteSwap : MonoBehaviour {
+	[SerializeField] private Sprite keyboardSprite;
+	[SerializeField] private Sprite gamepadSprite;
+
+	private SpriteRenderer spriteRenderer;
+	private PlayerInput playerInput;
+
+	private void OnEnable() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		GameObject player = GameObject.FindWithTag("Player");
+		if (player != null) {
+			playerInput = player.GetComponent<PlayerInput>();
+			playerInput.controlsChangedEvent.AddListener(OnControlsChanged);
+			OnControlsChanged(playerInput);
+		}
+	}
+
+	private void OnDisable() {
+		if (playerInput != null)
+			playerInput.controlsChangedEvent.RemoveListener(OnControlsChanged);
+	}
+
+	private void OnControlsChanged(PlayerInput _) {
+		spriteRenderer.sprite = playerInput.currentControlScheme == "Keyboard" ? keyboardSprite : gamepadSprite;
+	}
+}
