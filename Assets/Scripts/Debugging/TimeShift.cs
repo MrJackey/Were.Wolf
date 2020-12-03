16107dd82e18e32e6b8211802bc3d7093c65629a
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-03 17:06:09 +0100

    Add time shift script

---
+++ Assets/Scripts/Debugging/TimeShift.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TimeShift : MonoBehaviour {
+	[SerializeField, Range(0.01f, 2f)]
+	private float speed = 1f;
+
+	private void Update() {
+		if (Time.timeScale != 0)
+			Time.timeScale = speed;
+	}
+}
