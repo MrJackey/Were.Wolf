b23214756f283da52edce0c442906ec59b2b1c67
Adam Forsell <adafor2001@skola.goteborg.se>
2020-12-07 15:21:14 +0100

    $156 Edit Button Collider, $176 InteractionArrow for ExitDoor

--- Assets/EnterableGate.cs
+++ Assets/EnterableGate.cs
@@ -9,18 +9,24 @@ public class EnterableGate : Gate {
 	[SerializeField, Range(0.125f, 1f)]
 	private float enterThreshold = 0.5f;
 	[SerializeField] private UnityEvent onEnter;
+	[SerializeField] private UnityEvent onApproach;
+	[SerializeField] private UnityEvent onLeave;
 
 	private bool canEnter = false;
 	private bool isEntering = false;
 
 	private void OnTriggerEnter2D(Collider2D other) {
-		if (other.attachedRigidbody.CompareTag("Player") && !other.isTrigger)
+		if (other.attachedRigidbody.CompareTag("Player") && !other.isTrigger) {
 			canEnter = true;
+			onApproach.Invoke();
+		}
 	}
 
 	private void OnTriggerExit2D(Collider2D other) {
-		if (other.attachedRigidbody.CompareTag("Player") && !other.isTrigger)
+		if (other.attachedRigidbody.CompareTag("Player") && !other.isTrigger) {
 			canEnter = false;
+			onLeave.Invoke();
+		}
 	}
 
 	private void OnEnable() {
