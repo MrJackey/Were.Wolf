019473da5c0592f2fdd284f16affcf17a85b55bc
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-07 13:03:30 +0100

    Update CameraClearer.cs

--- Assets/Scripts/Camera/CameraClearer.cs
+++ Assets/Scripts/Camera/CameraClearer.cs
@@ -11,9 +11,11 @@ public class CameraClearer : MonoBehaviour {
 	}
 
 	private void OnBeginCameraRendering(ScriptableRenderContext ctx, Camera cam) {
-		CommandBuffer cmd = CommandBufferPool.Get("Camera Clearer");
+		CommandBuffer cmd = CommandBufferPool.Get();
 		try {
 			cmd.SetViewport(new Rect(0, 0, Screen.width, Screen.height));
+			// Unity is set up to automatically convert colors from linear space to sRGB. Because of this, we need to
+			// make sure we give it a color in linear space.
 			cmd.ClearRenderTarget(false, true, cam.backgroundColor.linear);
 
 			ctx.ExecuteCommandBuffer(cmd);
