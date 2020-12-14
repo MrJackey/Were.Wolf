5d9b231c18024aa702dd014480847740b704d098
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-14 11:13:21 +0100

    Add transformation form filter to speech messages

---
+++ Assets/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class GameObjectExtensions {
+	public static T GetComponentUnlessNull<T>(this GameObject gameObject) where T : Component {
+		return gameObject != null ? gameObject.GetComponent<T>() : null;
+	}
+
+	public static T GetComponentUnlessNull<T>(this Component component) where T : Component {
+		return component != null ? component.GetComponent<T>() : null;
+	}
+}
