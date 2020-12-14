9baed10e824df8c6ffb88b2d681270132ab5460f
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-14 16:26:29 +0100

    Add settings screen to main menu

---
+++ Assets/Scripts/Audio/VolumeSettingsLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSettingsLoader : MonoBehaviour {
+	[SerializeField] private SaveHelper saveHelper;
+	[SerializeField] private AudioMixer mixer;
+
+	private void Start() {
+		mixer.SetFloat("masterVolume", MathX.LinearToDecibels(saveHelper.Settings.masterVolume));
+		mixer.SetFloat("musicVolume", MathX.LinearToDecibels(saveHelper.Settings.musicVolume));
+		mixer.SetFloat("sfxVolume", MathX.LinearToDecibels(saveHelper.Settings.sfxVolume));
+	}
+}
---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+	[SerializeField] private SaveHelper saveHelper;
+	[SerializeField] private AudioMixer mixer;
+	[SerializeField] private Slider masterSlider;
+	[SerializeField] private Slider musicSlider;
+	[SerializeField] private Slider sfxSlider;
+
+	private void OnEnable() {
+		SaveManager.SettingsData settings = saveHelper.Settings;
+		masterSlider.value = settings.masterVolume;
+		musicSlider.value = settings.musicVolume;
+		sfxSlider.value = settings.sfxVolume;
+
+		masterSlider.onValueChanged.AddListener(OnMasterValueChanged);
+		musicSlider.onValueChanged.AddListener(OnMusicValueChanged);
+		sfxSlider.onValueChanged.AddListener(OnSFXValueChanged);
+	}
+
+	private void OnDisable() {
+		saveHelper.Save();
+
+		masterSlider.onValueChanged.RemoveListener(OnMasterValueChanged);
+		musicSlider.onValueChanged.RemoveListener(OnMusicValueChanged);
+		sfxSlider.onValueChanged.RemoveListener(OnSFXValueChanged);
+	}
+
+	private void OnMasterValueChanged(float value) {
+		saveHelper.Settings.masterVolume = value;
+		mixer.SetFloat("masterVolume", MathX.LinearToDecibels(value));
+	}
+
+	private void OnMusicValueChanged(float value) {
+		saveHelper.Settings.musicVolume = value;
+		mixer.SetFloat("musicVolume", MathX.LinearToDecibels(value));
+	}
+
+	private void OnSFXValueChanged(float value) {
+		saveHelper.Settings.sfxVolume = value;
+		mixer.SetFloat("sfxVolume", MathX.LinearToDecibels(value));
+	}
+}
