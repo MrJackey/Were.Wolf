b5cacf903bf6b662819d44ff7bb756fe49a238c2
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-14 13:33:01 +0100

    Add camera peek functionality

---
+++ Assets/Scripts/Player/CameraPeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraPeek : MonoBehaviour {
+	[SerializeField, Range(0.01f, 1f)]
+	private float inputThreshold = 0.9f;
+	[SerializeField] private float peekDistance;
+
+	private SnappingCamera snappingCamera;
+
+	private void Start() {
+		Camera mainCamera = Camera.main;
+		if (mainCamera != null)
+			snappingCamera = mainCamera.GetComponent<SnappingCamera>();
+	}
+
+	public void OnMoveInput(InputAction.CallbackContext ctx) {
+		float value = ctx.ReadValue<Vector2>().y;
+		snappingCamera.PeekOffset = Mathf.Abs(value) >= inputThreshold
+			? new Vector3(0, peekDistance * Mathf.Sign(value), 0)
+			: Vector3.zero;
+	}
+}
