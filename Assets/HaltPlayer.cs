d807570611f202727f28f10806c31d2946e1e8d8
Adam Forsell <adafor2001@skola.goteborg.se>
2020-12-15 15:50:32 +0100

    Applied decision
    Player now has to reach the left side of Waldu, to face the wall, before he emerges.

--- Assets/HaltPlayer.cs
+++ Assets/HaltPlayer.cs
@@ -6,54 +6,28 @@ public class HaltPlayer : MonoBehaviour {
 	[SerializeField] private PlayerController playerController;
 	[SerializeField] private Rigidbody2D playerRb2d;
 	[SerializeField] private Animator playerAnimator;
+	[SerializeField] private BoxCollider2D hasMetTrigger;
 
 	[SerializeField] private bool hasMetOnce = false;
-	[SerializeField] private bool doStopAnimator = false;
-	[SerializeField] private bool mustBeGrounded = true;
 
 	private void Start() {
 		hasMetOnce = false;
+		hasMetTrigger.enabled = false;
 	}
 
 	private void OnTriggerStay2D(Collider2D other) {
 
-	//####### Options for how to hold the player: ##############
-		if (mustBeGrounded) {
-			if (other.CompareTag("PlayerCollider") && playerController.IsGrounded && !hasMetOnce) {
-				playerRb2d.velocity = Vector2.zero;
-				playerController.enabled = false;
-				hasMetOnce = true;
+		if (other.CompareTag("PlayerCollider") && playerController.IsGrounded && !hasMetOnce) {
+			playerRb2d.velocity = Vector2.zero;
+			playerController.enabled = false;
+		
+			playerAnimator.SetFloat("speed", 0.0f);
+			playerAnimator.SetBool("isDashing", false);
+			playerAnimator.SetFloat("speedY", 0.0f);
 			
-				if (doStopAnimator) {
-					playerAnimator.enabled = false;
-				}			
-				else {
-					playerAnimator.SetFloat("speed", 0.0f);
-					playerAnimator.SetBool("isDashing", false);
-					playerAnimator.SetFloat("speedY", 0.0f);
-				}
-
-				this.enabled = false;
-			}		
-		}
-		// #########################################################	
-		else {
-			if (other.CompareTag("PlayerCollider") && !hasMetOnce) {
-				playerRb2d.velocity = Vector2.zero;
-				playerController.enabled = false;
-				hasMetOnce = true;
-
-				if (doStopAnimator) {
-					playerAnimator.enabled = false;
-				}
-				else {
-					playerAnimator.SetFloat("speed", 0.0f);
-					playerAnimator.SetBool("isDashing", false);
-					playerAnimator.SetFloat("speedY", 0.0f);
-				}	
-				
-				this.enabled = false;
-			}
+			hasMetTrigger.enabled = true;
+			hasMetOnce = true;
+			this.enabled = false;
 		}
 	}
 }
