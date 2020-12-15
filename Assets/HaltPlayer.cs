75b6719882bc1149fd5de4586953692682c1888c
Adam Forsell <adafor2001@skola.goteborg.se>
2020-12-15 13:07:13 +0100

    push push

--- Assets/HaltPlayer.cs
+++ Assets/HaltPlayer.cs
@@ -9,32 +9,51 @@ public class HaltPlayer : MonoBehaviour {
 
 	[SerializeField] private bool hasMetOnce = false;
 	[SerializeField] private bool doStopAnimator = false;
+	[SerializeField] private bool mustBeGrounded = true;
 
 	private void Start() {
 		hasMetOnce = false;
 	}
 
 	private void OnTriggerStay2D(Collider2D other) {
-		if (other.CompareTag("PlayerCollider") && playerController.IsGrounded && !hasMetOnce) {
-			playerRb2d.velocity = Vector2.zero;
-			playerController.enabled = false;
-			hasMetOnce = true;
+
+	//####### Options for how to hold the player: ##############
+		if (mustBeGrounded) {
+			if (other.CompareTag("PlayerCollider") && playerController.IsGrounded && !hasMetOnce) {
+				playerRb2d.velocity = Vector2.zero;
+				playerController.enabled = false;
+				hasMetOnce = true;
 			
-			//####### Options for how to hold the player: ##############
-				// ### 1: ###
 				if (doStopAnimator) {
 					playerAnimator.enabled = false;
-				}
-
-				// ### 2: ###
+				}			
 				else {
 					playerAnimator.SetFloat("speed", 0.0f);
 					playerAnimator.SetBool("isDashing", false);
 					playerAnimator.SetFloat("speedY", 0.0f);
-				}		
-			// #########################################################	
+				}
 
-			this.enabled = false;
+				this.enabled = false;
+			}		
+		}
+		// #########################################################	
+		else {
+			if (other.CompareTag("PlayerCollider") && !hasMetOnce) {
+				playerRb2d.velocity = Vector2.zero;
+				playerController.enabled = false;
+				hasMetOnce = true;
+
+				if (doStopAnimator) {
+					playerAnimator.enabled = false;
+				}
+				else {
+					playerAnimator.SetFloat("speed", 0.0f);
+					playerAnimator.SetBool("isDashing", false);
+					playerAnimator.SetFloat("speedY", 0.0f);
+				}	
+				
+				this.enabled = false;
+			}
 		}
 	}
 }
