876d16cb52a1aa237bd4ddcba393a3e1bcd29792
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-21 11:42:35 +0100

    Unlock levels based on completion

---
+++ Assets/Scripts/UI/LockLevelButtons.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LockLevelButtons : MonoBehaviour {
+	[SerializeField] private SaveHelper saveHelper;
+	[SerializeField] private Button[] buttons;
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+	[SerializeField] private bool onlyLockInRelease = true;
+#endif
+
+	private void OnEnable() {
+	#if UNITY_EDITOR || DEVELOPMENT_BUILD
+		if (onlyLockInRelease) return;
+	#endif
+
+		int maxLevel = saveHelper.CompletedLevel + 1;
+		for (int i = 0; i < buttons.Length; i++)
+			buttons[i].interactable = i <= maxLevel;
+	}
+}
