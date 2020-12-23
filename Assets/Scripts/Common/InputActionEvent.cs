37e7d75ba8b2f8ac8a0fbac1a0e5d1505fc1f7a0
MrJackey <jaclun2003@skola.goteborg.se>
2020-12-23 10:11:49 +0100

    Made cutscene skippable

--- Assets/Scripts/Common/InputActionEvent.cs
+++ Assets/Scripts/Common/InputActionEvent.cs
@@ -5,17 +5,25 @@ using UnityEngine.InputSystem;
 public class InputActionEvent : MonoBehaviour {
 	[SerializeField] private InputActionProperty action = default;
 	[SerializeField] private UnityEvent onStarted = null;
+	[SerializeField] private UnityEvent onPerformed = null;
+
 
 	private void OnEnable() {
 		action.action.started += OnStarted;
+		action.action.performed += OnPerformed;
 		action.action.Enable();
 	}
 
 	private void OnDisable() {
 		action.action.started -= OnStarted;
+		action.action.performed -= OnPerformed;
 	}
 
 	private void OnStarted(InputAction.CallbackContext ctx) {
 		onStarted.Invoke();
+	}
+
+	private void OnPerformed(InputAction.CallbackContext ctx) {
+		onPerformed.Invoke();
 	}
 }
