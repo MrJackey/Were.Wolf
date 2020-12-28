a7140bb953d04f268534c839a7c575be4c9dd522
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-28 09:21:41 +0100

    Implement custom cursor

---
+++ Assets/Scripts/Common/Utilities/SetCursor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetCursor : MonoBehaviour {
+	[SerializeField] private Texture2D texture = null;
+	[SerializeField] private Vector2 hotspot = Vector2.zero;
+	[SerializeField] private CursorMode mode = CursorMode.ForceSoftware;
+
+	private void Start() {
+		Cursor.SetCursor(texture, hotspot, mode);
+	}
+}
