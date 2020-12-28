d8738f39096fb1089e542825983bbc7d1f62383c
Jonatan Johansson <smeas@users.noreply.github.com>
2020-12-28 11:16:20 +0100

    Change button color on press

---
+++ Assets/Scripts/UI/ColorOnPressed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ColorOnPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	[SerializeField] private Image image;
+	[SerializeField] private Color pressedColor;
+
+	private Color normalColor;
+
+	private void Awake() {
+		normalColor = image.color;
+	}
+
+	public void OnPointerDown(PointerEventData eventData) {
+		image.color = pressedColor;
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {
+		image.color = normalColor;
+	}
+}
