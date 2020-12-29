f3dd004bc33c425aa83455f2c9637cf3fe7daf5c
Adam Forsell <adafor2001@skola.goteborg.se>
2020-12-29 10:54:48 +0100

    Removed Delay from Button Press

--- Assets/Scripts/Puzzle/PressureButton.cs
+++ Assets/Scripts/Puzzle/PressureButton.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PressureButton : SignalEmitter {
-	[SerializeField] BoxCollider2D colliderUp;
 	[SerializeField] private AudioSource audioSource;
-
-	private Animator animator;
-
-	public float delayTimer, delayEnd = 0.5f;
-	private float delayStart = 0;
-	private bool isDelayButtonUp = false, isDelayButtonDown = false;
-
+	[SerializeField] private Animator animator;
+	
 	public List<Collider2D> overlappingColliders = new List<Collider2D>();
 
 
@@ -19,23 +13,10 @@ public class PressureButton : SignalEmitter {
 		animator = GetComponent<Animator>();
 	}
 
-	private void Update() {
-		if (isDelayButtonDown && !IsActivated) {
-			delayTimer += Time.deltaTime;
-			PressureButtonDown();
-		}
-		else if (isDelayButtonUp && IsActivated) {
-			delayTimer += Time.deltaTime;
-			PressureButtonUp();
-		}
-	}
-
 	private void OnTriggerEnter2D(Collider2D other) {	
 		if (!other.isTrigger && (other.attachedRigidbody.CompareTag("Player") || other.attachedRigidbody.CompareTag("Box"))) {
 			overlappingColliders.Add(other);
-			delayTimer = delayStart;
-			isDelayButtonUp = false;
-			isDelayButtonDown = true;
+			PressureButtonDown();
 		}
 	}
 
@@ -43,30 +24,20 @@ public class PressureButton : SignalEmitter {
 		overlappingColliders.Remove(other);
 	
 		if (overlappingColliders.Count == 0) {
-			delayTimer = delayStart;
-			isDelayButtonDown = false;
-			isDelayButtonUp = true;
+			PressureButtonUp();
 		}
 	}
 
 	private void PressureButtonDown() {
-		if (delayTimer > delayEnd) {
-			PlaySound();
-			colliderUp.enabled = false;
-			animator.SetBool("IsPressed", true);
-			IsActivated = true;
-			isDelayButtonDown = false;
-		}
+		PlaySound();
+		animator.SetBool("IsPressed", true);
+		IsActivated = true;
 	}
 
 	private void PressureButtonUp() {
-		if (delayTimer > delayEnd) {
-			PlaySound();
-			colliderUp.enabled = true;
-			animator.SetBool("IsPressed", false);
-			IsActivated = false;
-			isDelayButtonUp = false;
-		}
+		PlaySound();
+		animator.SetBool("IsPressed", false);
+		IsActivated = false;
 	}
 
 	public void PlaySound() {
