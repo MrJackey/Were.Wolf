7dfaa34d7a87fafa1ddb5f266f4fa2aab180c3c1
MrJackey <jaclun2003@skola.goteborg.se>
2020-12-29 17:02:20 +0100

    Made pressurebuttons still on awake

--- Assets/Scripts/Puzzle/PressureButton.cs
+++ Assets/Scripts/Puzzle/PressureButton.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PressureButton : SignalEmitter {
+	private static readonly int pressHash = Animator.StringToHash("press");
+	private static readonly int releaseHash = Animator.StringToHash("release");
+
 	[SerializeField] private AudioSource audioSource;
 	[SerializeField] private Animator animator;
-	
+
 	public List<Collider2D> overlappingColliders = new List<Collider2D>();
 
-
-	private void OnTriggerEnter2D(Collider2D other) {	
+	private void OnTriggerEnter2D(Collider2D other) {
 		if (!other.isTrigger && (other.attachedRigidbody.CompareTag("Player") || other.attachedRigidbody.CompareTag("Box"))) {
 			overlappingColliders.Add(other);
 			PressureButtonDown();
@@ -18,7 +20,7 @@ public class PressureButton : SignalEmitter {
 
 	private void OnTriggerExit2D(Collider2D other) {
 		overlappingColliders.Remove(other);
-	
+
 		if (overlappingColliders.Count == 0) {
 			PressureButtonUp();
 		}
@@ -27,14 +29,14 @@ public class PressureButton : SignalEmitter {
 	private void PressureButtonDown() {
 		if (IsActivated) return;
 		PlaySound();
-		animator.SetBool("IsPressed", true);
+		animator.SetTrigger(pressHash);
 		IsActivated = true;
 	}
 
 	private void PressureButtonUp() {
 		if (!IsActivated) return;
 		PlaySound();
-		animator.SetBool("IsPressed", false);
+		animator.SetTrigger(releaseHash);
 		IsActivated = false;
 	}
 
