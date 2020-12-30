5aa09ea223bd0d211d3a28b4df1047b9d891d164
MrJackey <jaclun2003@skola.goteborg.se>
2020-12-30 17:04:14 +0100

    Added credits screen to main menu and outro cutscene

---
+++ Assets/Scripts/Utils/DelayedCall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DelayedCall : MonoBehaviour {
+	[SerializeField] private int numberOfFrames = 1;
+	[SerializeField] private UnityEvent onDelayDone;
+
+	public void StartDelay() => StartCoroutine(CoDelay());
+
+	private IEnumerator CoDelay() {
+		for (int i = 0; i < numberOfFrames; i++) {
+			yield return null;
+		}
+
+		onDelayDone.Invoke();
+	}
+}
