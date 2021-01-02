0d7ca502aeceed6bdf0375a5bac05d5924c75d63
Jonatan Johansson <smeas@users.noreply.github.com>
2021-01-02 19:34:38 +0100

    Add activation event to checkpoint

--- Assets/Scripts/Common/Checkpoint.cs
+++ Assets/Scripts/Common/Checkpoint.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class Checkpoint : MonoBehaviour {
 	[SerializeField] private bool isActivated;
+	[SerializeField] private UnityEvent onActivated;
 
 	public bool IsActivated => isActivated;
 
+	private void Start() {
+		if (isActivated)
+			onActivated.Invoke();
+	}
+
 	private void OnTriggerEnter2D(Collider2D other) {
-		if (other.attachedRigidbody.CompareTag("Player"))
-			isActivated = true;
+		if (other.attachedRigidbody.CompareTag("Player")) {
+			if (!isActivated) {
+				isActivated = true;
+				onActivated.Invoke();
+			}
+		}
 	}
 }
